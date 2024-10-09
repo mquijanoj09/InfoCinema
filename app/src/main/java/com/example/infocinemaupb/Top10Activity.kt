b3ff4022e57fed7a901c4339d8f58cd04daf4fd7
mquijanoj09 <mquijanoj@hotmail.com>
2024-10-08 21:39:14 -0500

    Comienzo barra de navegacion

---
+++ app/src/main/java/com/example/infocinemaupb/Top10Activity.kt
@@ -0,0 +1,11 @@
+package com.example.infocinema
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Top10Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top10)
+    }
+}
